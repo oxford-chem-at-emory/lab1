e9285367f2153aefe7655a6d806d9b7e133ce94e
oxford-chem-at-emory <oxford.chemistry.at.emory@gmail.com>
2024-02-27 06:19:57 +0900

    numpy + numpy with 5 dp

---
+++ test_0226.py
@@ -0,0 +1,15 @@
+import numpy as np
+# 1000, 500, 1 -> integer
+a = 1000
+b = 100
+c = 1
+# 2.0, 1.00004 -> float
+d = 2.0
+division = a / d
+print(f"{division:.5f}")
+#print(f" {result:.5e}")
+#pip install numpy
+
+
+
+
---
+++ test_0226b.py
@@ -0,0 +1,4 @@
+import numpy as np
+print(f"{np.sqrt(4):.5f}")
+#/Library/Frameworks/Python.framework/Versions/3.12/bin/python3 
+#/Users/mo/Desktop/Test/.vscode/test_0226b.py
